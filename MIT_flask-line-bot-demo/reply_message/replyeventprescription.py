41e7fe99d5d1ec71dc0b5d28eeb47ac359005da8
NullVoided <86790807+NullVoided@users.noreply.github.com>
2021-07-03 01:26:39 +0800

    Just need to add an input module

--- MIT_flask-line-bot-demo/reply_message/replyeventprescription.py
+++ MIT_flask-line-bot-demo/reply_message/replyeventprescription.py
@@ -39,7 +39,15 @@ if channel_access_token is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 line_bot_api = LineBotApi(channel_access_token)
-output = FlexSendMessage(
+@handler.add(MessageEvent, message=TextMessage)
+#this is the input part for the user to enter their perscription info 
+
+
+#this is the command to check perscription
+def message_text(event):
+    message = event.message.text
+    if message == 'Check Prescription':
+        output = FlexSendMessage(
             alt_text='hello',
             contents={
   "type": "carousel",
@@ -53,7 +61,7 @@ output = FlexSendMessage(
         "contents": [
           {
             "type": "text",
-              #this is the first med the user enters on the input part
+            "text": med[0],   #this is the first med the user enters on the input part
             "color": "#ffffff",
             "align": "start",
             "size": "md",
@@ -105,9 +113,10 @@ output = FlexSendMessage(
             "contents": [
               {
                 "type": "text",
-               #this is the perscription method eg. 2 tablets after meals a  day
+                "text": howto[0], #this is the perscription method eg. 2 tablets after meals a  day
                 "color": "#8C8C8C",
                 "size": "sm",
+                "wrap": true
               }
             ],
             "flex": 1
@@ -118,6 +127,7 @@ output = FlexSendMessage(
       },
       "styles": {
         "footer": {
+          "separator": false
         }
       }
     },
@@ -130,7 +140,7 @@ output = FlexSendMessage(
         "contents": [
           {
             "type": "text",
-            
+            "text": med[1],
             "color": "#ffffff",
             "align": "start",
             "size": "md",
@@ -182,9 +192,10 @@ output = FlexSendMessage(
             "contents": [
               {
                 "type": "text",
-           
+                "text": howto[1],
                 "color": "#8C8C8C",
                 "size": "sm",
+                "wrap": true
               }
             ],
             "flex": 1
@@ -195,6 +206,7 @@ output = FlexSendMessage(
       },
       "styles": {
         "footer": {
+          "separator": false
         }
       }
     },
@@ -207,7 +219,7 @@ output = FlexSendMessage(
         "contents": [
           {
             "type": "text",
-         
+            "text": med[2],
             "color": "#ffffff",
             "align": "start",
             "size": "md",
@@ -259,9 +271,10 @@ output = FlexSendMessage(
             "contents": [
               {
                 "type": "text",
-              
+                "text": howto[2],
                 "color": "#8C8C8C",
                 "size": "sm",
+                "wrap": true
               }
             ],
             "flex": 1
@@ -272,12 +285,232 @@ output = FlexSendMessage(
       },
       "styles": {
         "footer": {
+          "separator": false
         }
       }
     }
   ]
 }
+                }
+            }
         )
+     elif message == 'Medicine Reciept': #the receipt for the medicine
+     output = FlexSendMessage(
+     alt_text='hello',
+     contents={
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "RECEIPT",
+        "weight": "bold",
+        "color": "#1DB446",
+        "size": "sm"
+      },
+      {
+        "type": "text",
+        "text": "Pharmacy ",
+        "weight": "bold",
+        "size": "xxl",
+        "margin": "md"
+      },
+      {
+        "type": "text",
+        "size": "xs",
+        "color": "#aaaaaa",
+        "wrap": true,
+        "text": "-----------------------------------"
+      },
+      {
+        "type": "separator",
+        "margin": "xxl"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "margin": "xxl",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": med[0],
+                "size": "sm",
+                "color": "#555555",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": price[0], # price of the first medicine
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": med[1],
+                "size": "sm",
+                "color": "#555555",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": price[1],
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": med[2],
+                "size": "sm",
+                "color": "#555555",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": price[2],
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "separator",
+            "margin": "xxl"
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "margin": "xxl",
+            "contents": [
+              {
+                "type": "text",
+                "text": "ITEMS",
+                "size": "sm",
+                "color": "#555555"
+              },
+              {
+                "type": "text",
+                "text": "3",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "TOTAL",
+                "size": "sm",
+                "color": "#555555"
+              },
+              {
+                "type": "text",
+                "text": price[1]+price[0]+price[2],
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "CASH",
+                "size": "sm",
+                "color": "#555555"
+              },
+              {
+                "type": "text",
+                "text": money,
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "CHANGE",
+                "size": "sm",
+                "color": "#555555"
+              },
+              {
+                "type": "text",
+                "text": "$"+40-(price[1]+price[0]+price[2]),
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "type": "separator",
+        "margin": "xxl"
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "margin": "md",
+        "contents": [
+          {
+            "type": "text",
+            "text": "PAYMENT ID",
+            "size": "xs",
+            "color": "#aaaaaa",
+            "flex": 0
+          },
+          {
+            "type": "text",
+            "text": "#42069123774",
+            "color": "#aaaaaa",
+            "size": "xs",
+            "align": "end"
+          }
+        ]
+      }
+    ]
+  },
+  "styles": {
+    "footer": {
+      "separator": true
+    }
+  }
+}
+    )
 e1 = 'U82f489e2fc2bfc92f9545281e7beda30'
 line_bot_api.push_message(
           e1,
